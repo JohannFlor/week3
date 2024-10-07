45eefa37138243bc5316b2e6d8db9f553c14aad0
JohannFlor <159105070+JohannFlor@users.noreply.github.com>
2024-10-06 20:16:40 -0500

    advances

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
+const routerCountry = require('./country.router');
 const router = express.Router();
 
 // colocar las rutas aquí
-
-
+router.use('/country', routerCountry)
 module.exports = router;
